5d62860f48e8a52de486c541d7f30b3f3534cf6c
DimitarMilkovski <117305579+DimitarMilkovski@users.noreply.github.com>
2022-12-07 17:39:23 +0100

    Add files via upload

---
+++ Homework1/script.js
@@ -0,0 +1,8 @@
+
+var phonesNumber = 30;
+var phonePrice = 119.95;
+var taxRate = 1.05;
+
+var totalPrice = (phonesNumber*phonePrice)*taxRate;
+
+console.log(`The total price of ${phonesNumber} phones with single price of ${phonePrice} is equal to ${totalPrice} dollars`);
